a28f1055e020a8c612efc08b86513f204761f9a6
RanBiron <1748537446@qq.com>
2020-03-20 18:23:10 +0800

    更改版本信息

--- source/conf.py
+++ source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'TxBook'
-copyright = '2019, RanBiron'
-author = 'RanBiron'
+project = 'Extern Module'
+copyright = '2019, HuoMChen'
+author = 'HuoMChen'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
